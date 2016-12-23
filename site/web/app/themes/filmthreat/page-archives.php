cb5995430a1445c11c5777cd4f48a02a20543d2f
Warren Curtis <warren.curtis@iamplus.com>
2016-12-22 21:58:46 -0800

    update css and update prod vault files

---
+++ site/web/app/themes/filmthreat/page-archives.php
@@ -0,0 +1,29 @@
+<?php
+/*
+    Template Name: Archives
+*/
+?>
+
+<?php while (have_posts()) : the_post(); ?>
+  <?php get_template_part('templates/page', 'header'); ?>
+
+<?php endwhile; ?>
+
+<div class="row">
+<?php
+query_posts('cat=11');
+while(have_posts()) : the_post();
+?>
+
+
+  <div class="col-sm-4">
+    <a href="<?php the_permalink(); ?>"><img width="350" src="<?php the_field('teaser_image'); ?>"></a>
+    <p class="review-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></p>
+    <div><?php if ( function_exists( 'ADDTOANY_SHARE_SAVE_KIT' ) ) { ADDTOANY_SHARE_SAVE_KIT(); } ?></div>
+  </div>
+
+
+<?php endwhile;
+wp_reset_query();
+?>
+</div>
