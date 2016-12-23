7b53cadbacd5072fedf4490087b2a864720bfc57
Warren Curtis <warren.curtis@iamplus.com>
2016-12-22 22:18:43 -0800

    add search to header

--- site/web/app/themes/filmthreat/templates/header.php
+++ site/web/app/themes/filmthreat/templates/header.php
@@ -2,11 +2,11 @@
 <header class="banner">
   <div class="container">
     <div class="row">
-      <div class="col-xs-4">
+      <div class="col-sm-4">
 
     <a class="brand" href="<?= esc_url(home_url('/')); ?>"><img class="logo" src="/app/uploads/2016/12/film-threat-logo-1.png"></a>
       </div>
-  <div class="col-xs-8">
+  <div class="col-sm-6">
     <nav class="nav-primary">
       <?php
       if (has_nav_menu('primary_navigation')) :
@@ -14,6 +14,14 @@
       endif;
       ?>
     </nav>
+  </div>
+  <div class="col-sm-2">
+    <form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
+    <div>
+        <input style="width: 120px; margin-top: 15px;" type="text" value="" placeholder="search" name="s" id="s" />
+        <button class="search-btn" type="submit"><i class="fa fa-search"></i></button>
+    </div>
+</form>
   </div>
 </div>
   </div>
