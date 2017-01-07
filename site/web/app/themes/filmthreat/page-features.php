79ae9b37a9dbd96aed8ed994404285e2080dea2b
Warren Curtis <warren.curtis@iamplus.com>
2017-01-07 16:10:51 -0500

    add new plugins and update css

--- site/web/app/themes/filmthreat/page-features.php
+++ site/web/app/themes/filmthreat/page-features.php
@@ -3,7 +3,27 @@
     Template Name: Features
 */
 ?>
+
 <?php while (have_posts()) : the_post(); ?>
   <?php get_template_part('templates/page', 'header'); ?>
-  <?php get_template_part('templates/content', 'page'); ?>
+
 <?php endwhile; ?>
+
+<div class="row">
+<?php
+query_posts('cat=5,-1');
+while(have_posts()) : the_post();
+?>
+
+
+  <div class="col-sm-4">
+    <a href="<?php the_permalink(); ?>"><img width="350" src="<?php the_field('teaser_image'); ?>"></a>
+    <p class="review-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></p>
+    <div><?php if ( function_exists( 'ADDTOANY_SHARE_SAVE_KIT' ) ) { ADDTOANY_SHARE_SAVE_KIT(); } ?></div>
+  </div>
+
+
+<?php endwhile;
+wp_reset_query();
+?>
+</div>
--- site/web/app/themes/filmthreat/review.php
+++ site/web/app/themes/filmthreat/review.php
@@ -1,6 +1,6 @@
 <?php
 /*
-    Template Name: Review
+    Template Name: Review Single
 */
 ?>
 
