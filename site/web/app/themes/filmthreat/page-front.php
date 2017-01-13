3595895327a2a016959e5a88d2d6fa8442d9f6d1
Warren Curtis <warren.curtis@iamplus.com>
2017-01-12 17:08:05 -0800

    front page updates search css

--- site/web/app/themes/filmthreat/page-front.php
+++ site/web/app/themes/filmthreat/page-front.php
@@ -35,7 +35,6 @@ wp_reset_query();
 
 <?php
 $current_year = get_the_date('Y')+1;
-echo $current_year;
 query_posts('posts_per_page=40&cat=-7,-1&year=' . $current_year);
 while(have_posts()) : the_post();
 ?>
