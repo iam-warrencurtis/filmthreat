e97d67c850adc91948ab6ac355a4ed50d6fe7e87
Warren Curtis <warren.curtis@iamplus.com>
2017-04-01 16:43:31 -0700

    update home page

--- site/web/app/themes/filmthreat/page-front.php
+++ site/web/app/themes/filmthreat/page-front.php
@@ -25,35 +25,3 @@ while(have_posts()) : the_post();
 <?php endwhile;
 wp_reset_query();
 ?>
-
-<div class="home-articles">
-<div class="row">
-<div class="col-sm-12">
-  <!-- <h1 class="latest-articles">Latest Articles</h1> -->
-</div>
-
-
-<?php
-$current_year = get_the_date('Y')+1;
-query_posts('posts_per_page=40&cat=-7,-1&year=' . $current_year);
-while(have_posts()) : the_post();
-?>
-
-
-  <!-- <div class="col-sm-4">
-    <a href="<?php the_permalink(); ?>"><img width="350" src="<?php the_field('teaser_image'); ?>"></a>
-    <div class="home-article-category"><p>
-      <?php
-        $category = get_the_category();
-        echo $category[0]->cat_name;
-      ?></p>
-    </div>
-    <p class="home-article-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></p>
-  </div> -->
-
-
-<?php endwhile;
-wp_reset_query();
-?>
-</div>
-</div>
