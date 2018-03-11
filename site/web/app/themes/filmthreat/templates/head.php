f0771cdf63f2c4095bc0de7f7e54ab6d04f179c5
Warren Curtis <warren.curtis@iamplus.com>
2018-03-11 09:21:28 -0700

    add captcha

--- site/web/app/themes/filmthreat/templates/head.php
+++ site/web/app/themes/filmthreat/templates/head.php
@@ -5,4 +5,5 @@
   <?php wp_head(); ?>
   <link href="https://fonts.googleapis.com/css?family=Lato:300,400|Roboto|Roboto+Slab:400,700" rel="stylesheet">
   <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+<script src='https://www.google.com/recaptcha/api.js'></script>
 </head>
