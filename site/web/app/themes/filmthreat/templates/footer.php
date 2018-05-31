df36a76ba77d4ecb784717ebf52127d7d4f05502
Warren Curtis <warren.curtis@iamplus.com>
2018-05-30 21:55:54 -0700

    add redirect

--- site/web/app/themes/filmthreat/templates/footer.php
+++ site/web/app/themes/filmthreat/templates/footer.php
@@ -42,5 +42,11 @@
   </div>
 </footer>
 
+<script>
+document.addEventListener( 'wpcf7mailsent', function( event ) {
+    location = '/thank-you';
+}, false );
+</script>
+
 <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.6/js/bootstrap.min.js"></script>
 <script src="/app/themes/filmthreat/assets/scripts/script.js"></script>
