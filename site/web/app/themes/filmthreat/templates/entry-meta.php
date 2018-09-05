63dd669fdeaf4da5efee5dfd3681e7183056c421
Warren Curtis <warren.curtis@iamplus.com>
2018-09-05 08:29:47 -0700

    update meta for share

--- site/web/app/themes/filmthreat/templates/entry-meta.php
+++ site/web/app/themes/filmthreat/templates/entry-meta.php
@@ -1,2 +1,2 @@
-<h2 class="byline author vcard"><a href="<?= get_author_posts_url(get_the_author_meta('ID')); ?>" rel="author" class="fn">By <?= get_the_author(); ?></a></h2>
+<h2 class="byline author vcard"><?php if( get_field('author_alias') ): ?>by <?php the_field('author_alias'); ?><?php endif; ?>" rel="author" class="fn">By <?= get_the_author(); ?></a></h2>
 <time class="updated" datetime="<?= get_post_time('c', true); ?>"><?= get_the_date(); ?></time>
